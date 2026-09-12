b8d6cd1c965300c56e9614b628c33a043da7baa8
MichaelGaede-GER <info@michaelgaede.de>
2026-09-12 23:43:46 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 
 window.CONFIG = {
   // ---- App-Version (bei jedem Deploy hochzählen) ------------------
-  APP_VERSION: '51',
+  APP_VERSION: '52',
 
   // ---- Supabase ---------------------------------------------------
   SUPABASE_URL: 'https://mfgxrnvwrirvjjanigul.supabase.co',
